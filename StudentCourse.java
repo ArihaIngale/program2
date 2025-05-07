d1f55946a79ca203c346bd84d9e44efff2ee0a50
ArihaIngale <arihaingale220705@gmail.com>
2025-05-07 16:59:39 +0530

    course

---
+++ StudentCourse.java
@@ -0,0 +1,12 @@
+public class StudentCourse{
+    String[] courses = {"Math", "Physics", "CS"};
+    int[] marks = {78, 65, 92};
+
+    public void displayCourses() {
+        for (int i = 0; i < courses.length; i++) {
+            System.out.println(courses[i] + ": " + marks[i]);
+        }
+    }
+}
+
+
